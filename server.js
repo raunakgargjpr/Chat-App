246a4b4cc3ce9ac606de76cd41ca2233308186e4
raunakgargjpr <112203116+raunakgargjpr@users.noreply.github.com>
2024-10-16 22:55:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const path = require("path");
-const http = require("http");
+const https = require("https");
 const express = require("express");
 const socketio = require("socket.io");
 const formatMessage = require("./utils/messages");
 const {userJoin,getCurrentUser,userLeave,getRoomUsers} = require("./utils/users");
 
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(app);
 const io = socketio(server);
 
 // Set static folder
